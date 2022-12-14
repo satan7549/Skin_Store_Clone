27f6cf4aa8ff54c65cfa0bc1b841920a8c5deea1
Satan Sharma <satansharma7549@gmail.com>
2022-12-14 23:33:58 +0530

    Add

--- skin-store-app/src/Components/Navbar.jsx
+++ skin-store-app/src/Components/Navbar.jsx
@@ -1,10 +1,56 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "../style/navbar.css";
+import { Button, Input, useDisclosure } from "@chakra-ui/react";
+import {
+  Drawer,
+  DrawerBody,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+} from "@chakra-ui/react";
 
 export const Navbar = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = React.useRef();
+
   return (
-    <div className='nav' >
+    // <div className="nav"  >
+    <>
+      {/* <Button ref={btnRef}  color="teal" onClick={onOpen}> */}
+      <FontAwesomeIcon className="headerIcons" ref={btnRef}  color="teal" onClick={onOpen} icon={faBars} />
+      {/* open
+      </Button> */}
+      <Drawer
+        isOpen={isOpen}
+        placement="left"
+        onClose={onClose}
+        finalFocusRef={btnRef}
+        size="lg"
+        border="1px solid red"
         
-    </div>
-  )
-}
+      >
+        <DrawerOverlay />
+        <DrawerContent  >
+          <DrawerCloseButton  />
+          <DrawerHeader>Create your account</DrawerHeader>
+
+          <DrawerBody>
+            <Input placeholder="Type here..." />
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+      
+      {/* <Link to="/">
+        <FontAwesomeIcon className="headerIcons" icon={faBars} />
+      </Link> */}
+      {/* <Link to="/">search</Link>
+      <Link to="/">Home</Link>
+      <Link to="/">Home</Link> */}
+      </>
+    // </div>
+  );
+};
--- skin-store-app/src/Routes/AllRoutes.jsx
+++ skin-store-app/src/Routes/AllRoutes.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
-import { Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
+import { Cart } from '../Pages/Cart'
+import { ErrorPage } from '../Pages/ErrorPage'
+import { Home } from '../Pages/Home'
+import { Login } from '../Pages/Login'
 
 export const AllRoutes = () => {
   return (
     <div>
         <Routes>
-            
+            <Route path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/cart' element={<Cart />} />
+            <Route path="/error" element={<ErrorPage />} /> 
         </Routes>
     </div>
   )
