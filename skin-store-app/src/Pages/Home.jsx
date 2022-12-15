f6129727d4cadd1de9b3dac7fe57a8ab5b3141c9
Satan Sharma <satansharma7549@gmail.com>
2022-12-15 23:25:30 +0530

    Update Home.jsx

--- skin-store-app/src/Pages/Home.jsx
+++ skin-store-app/src/Pages/Home.jsx
@@ -1,16 +1,14 @@
-import React, { useState } from 'react'
-import { useEffect } from 'react'
-import { Carasoul } from '../Components/Carasoul'
+import React, { useEffect, useState } from 'react'
 import { fetchDataBySearch } from '../data/fetchData'
 
 export const Home = () => {
-//   const [data,setData]=useState([]);
-//   useEffect(()=>{
-//     fetchDataBySearch().then((res)=>setData(res.data))
-//   },[])
-// console.log(data);
+  const [data,setData]=useState([]);
+  useEffect(()=>{
+    fetchDataBySearch().then((res)=>setData(res.data));
+  },[])
 
+  console.log(data);
   return (
-    <Carasoul />
+   <div>Home</div>
   )
 }
