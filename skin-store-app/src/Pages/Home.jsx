3670a8fb3866b4bd10d87a663e238a9daf34827d
Satan Sharma <satansharma7549@gmail.com>
2022-12-17 00:05:39 +0530

    product Data

--- skin-store-app/src/Pages/Home.jsx
+++ skin-store-app/src/Pages/Home.jsx
@@ -1,21 +1,20 @@
 import React, { useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
-import IndividualIntervalsExample from "../Components/Carasoul";
+import CardComp from "../Components/CardComp";
+// import IndividualIntervalsExample from "../Components/Carasoul";
 import { CategoryComp } from "../Components/CategoryComp";
 import { fetchDataBySearch } from "../data/fetchData";
+import { Products } from "./Products";
 
 export const Home = () => {
-  // const [data,setData]=useState([]);
-  // useEffect(()=>{
-  //   fetchDataBySearch().then((res)=>setData(res.data));
-  // },[])
 
-  // console.log(data);
+
   return (
     // <Container>
-    //   <CategoryComp />
-      <IndividualIntervalsExample />
+    // <CategoryComp /> 
+    //   {/* <IndividualIntervalsExample /> */}
     // </Container>
-    //  <ControlledCarousel />
+    <Products />
+    
   );
 };
--- skin-store-app/src/Pages/Products.jsx
+++ skin-store-app/src/Pages/Products.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Box } from '@chakra-ui/react'
+import  CardComp from "../Components/CardComp"
+import { fetchAllProducts, SearchByCategory } from '../data/fetchData';
 
 export const Products = () => {
+  // const {type}=props;
+  // console.log(type);
+  const [productType,setProductTtype]=useState([]);
+
+
+// useEffect(()=>{
+//   try{
+//     SearchByCategory(type).then((res)=>setProductTtype(res.data))
+//   }catch(error){
+//   console.log('error:', error)
+//   }
+  
+// },[type])
+
+console.log(productType);
+
   return (
-    <div>Products</div>
+    <Box display="grid" gap="10px" p="10px" gridTemplateColumns="repeat(4,1fr)" gridTemplateRows="auto" >
+      {
+        productType.map((ele)=><CardComp key={ele.id} item={ele} /> )
+      }
+      
+    </Box>
   )
 }
