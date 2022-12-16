bd529274e9466332bfac430ae22cc11c83cd085e
Satan Sharma <satansharma7549@gmail.com>
2022-12-16 18:38:16 +0530

    Update fetchData.js

--- skin-store-app/src/data/fetchData.js
+++ skin-store-app/src/data/fetchData.js
@@ -1,6 +1,8 @@
-import axios from "axios"
+import axios from "axios";
 //get Data from search {query}
 
-export const fetchDataBySearch =async (query) => {
-   return axios.get(`https://backenddataskinproducts-production.up.railway.app/api/skinproducts?q=${query}`) 
-}
+export const fetchDataBySearch = async (query) => {
+  return axios.get(
+    `https://backenddataskinproducts-production.up.railway.app/api/skinproducts?q=${query}`
+  );
+};
