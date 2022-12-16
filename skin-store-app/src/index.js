90dd9e4caf7dc8c13ccd683b4253ec728506954f
Satan Sharma <satansharma7549@gmail.com>
2022-12-16 18:38:23 +0530

    Update index.js

--- skin-store-app/src/index.js
+++ skin-store-app/src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
-import AuthContextProvider from "./Context/AuthContextProvider";
+import AuthContextProvider from "./Context/Auth/AuthContextProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
