82e3a8b617981e061fe624f0acf301b5de2542cf
Satan Sharma <satansharma7549@gmail.com>
2022-12-16 18:37:54 +0530

    Create AuthContextProvider.jsx

---
+++ skin-store-app/src/Context/Auth/AuthContextProvider.jsx
@@ -0,0 +1,24 @@
+import { createContext, useReducer } from "react";
+import reducer from "./Reducer";
+
+export const AuthContext = createContext();
+
+const initialState = {
+  isLoading: false,
+  isAuth: false,
+  isError: false,
+  token: null,
+};
+
+const AuthContextProvider = ({ children }) => {
+  // console.log(reducer);
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <AuthContext.Provider value={{ state, dispatch }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContextProvider;
