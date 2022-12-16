a93f656f84a270391fa27b962e91651b8842cdec
Satan Sharma <satansharma7549@gmail.com>
2022-12-16 18:38:13 +0530

    Create Reducer.js

---
+++ skin-store-app/src/Context/Auth/Reducer.js
@@ -0,0 +1,41 @@
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "LOGIN_LOADIN": {
+      return {
+        isLoading: true,
+        isAuth: false,
+        isError: false,
+        token: null,
+      };
+    }
+    case "LOGIN_SUCESS": {
+      return {
+        isLoading: false,
+        isAuth: true,
+        isError: false,
+        token: action.payload,
+      };
+    }
+    case "LOGIN_FAILURE": {
+      return {
+        isLoading: false,
+        isAuth: false,
+        isError: true,
+        token: null,
+      };
+    }
+    case "LOGOUT_SUCESS": {
+      return {
+        isLoading: false,
+        isAuth: false,
+        isError: false,
+        token: null,
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+};
+
+export default reducer;
