c7468c108cfbca0da1cf69675ae0d0e18d6d84f6
Satan Sharma <satansharma7549@gmail.com>
2022-12-16 18:37:58 +0530

    Create Action.js

---
+++ skin-store-app/src/Context/Auth/Action.js
@@ -0,0 +1,16 @@
+
+
+export const loginSucessAction = (data) => ({
+  type: "LOGIN_SUCESS",
+  payload: data,
+});
+
+
+export const logoutSucessAction = () => ({
+  type: "LOGOUT_SUCESS",
+});
+
+
+export const loginFailureAction = () => ({
+  type: "LOGIN_FAILURE",
+});
