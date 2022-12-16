5315ea7d51f0b41690bf1bd74ea6e1daf9b4c81b
Satan Sharma <satansharma7549@gmail.com>
2022-12-17 03:55:20 +0530

    update

--- skin-store-app/src/Pages/Cart.jsx
+++ skin-store-app/src/Pages/Cart.jsx
@@ -1,5 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
+import { CartContext } from "../Context/Cart/CartContextProvider";
 
 export const Cart = () => {
+  const {state}=useContext(CartContext);
+//next work we have to store data some ware because we find only one data heare 
+
+  console.log("CartPage",state.cart);
   return <div>Cart</div>;
 }
--- skin-store-app/src/Pages/Products.jsx
+++ skin-store-app/src/Pages/Products.jsx
@@ -1,31 +1,18 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { Box } from '@chakra-ui/react'
 import  CardComp from "../Components/CardComp"
 import { fetchAllProducts, SearchByCategory } from '../data/fetchData';
+import { ProductContext } from '../Context/ProductContext/ProductContext';
 
 export const Products = () => {
-  // const {type}=props;
-  // console.log(type);
-  const [productType,setProductTtype]=useState([]);
-
-
-// useEffect(()=>{
-//   try{
-//     SearchByCategory(type).then((res)=>setProductTtype(res.data))
-//   }catch(error){
-//   console.log('error:', error)
-//   }
+  const {state}=useContext(ProductContext);
+  const productType=state.categoryProduct
   
-// },[type])
-
-console.log(productType);
-
   return (
     <Box display="grid" gap="10px" p="10px" gridTemplateColumns="repeat(4,1fr)" gridTemplateRows="auto" >
       {
         productType.map((ele)=><CardComp key={ele.id} item={ele} /> )
       }
-      
     </Box>
   )
 }
