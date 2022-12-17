44c87ec9347698c9e59af7a7d9dc4b40cd40988e
Satan Sharma <satansharma7549@gmail.com>
2022-12-17 23:59:03 +0530

    Update CartContextProvider.jsx

--- skin-store-app/src/Context/Cart/CartContextProvider.jsx
+++ skin-store-app/src/Context/Cart/CartContextProvider.jsx
@@ -1,16 +1,20 @@
 import { createContext, useReducer } from "react";
-import reducer from "./cartReducer"
+import reducer from "./cartReducer";
 export const CartContext = createContext();
 
-const initialState={
-  cart:[],
-  total_item:"",
-  total_amount:""
-}
+const initialState = {
+  cart: [],
+  total_item: "",
+  total_amount: "",
+};
 
 const CartContextProvide = ({ children }) => {
-  const [state,dispatch]=useReducer(reducer,initialState);
-    
-  return <CartContext.Provider value={{state,dispatch}} >{children}</CartContext.Provider>;
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <CartContext.Provider value={{ state, dispatch }}>
+      {children}
+    </CartContext.Provider>
+  );
 };
-export default CartContextProvide
+export default CartContextProvide;
