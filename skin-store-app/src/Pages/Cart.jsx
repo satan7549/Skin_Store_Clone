d6b69ef5460ef2dee70ca81330f6c5250941bb02
Satan Sharma <satansharma7549@gmail.com>
2022-12-17 21:35:17 +0530

    Update Cart.jsx

--- skin-store-app/src/Pages/Cart.jsx
+++ skin-store-app/src/Pages/Cart.jsx
@@ -1,10 +1,95 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { CartContext } from "../Context/Cart/CartContextProvider";
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+  Image,
+  Text,
+  Box,
+  CloseButton,
+} from "@chakra-ui/react";
+import QuantityBtn from "../Components/QuantyBtn";
+// import { CartCard } from "../Components/CartCard";
 
 export const Cart = () => {
-  const {state}=useContext(CartContext);
-//next work we have to store data some ware because we find only one data heare 
+  const [totalQuantity,setTotalQuantity]=useState(0);
+  const { state,dispatch } = useContext(CartContext);
+  //next work we have to store data some ware because we find only one data heare
 
-  console.log("CartPage",state.cart);
-  return <div>Cart</div>;
+  console.log("CartPage", state.cart);
+
+const handleTotalCount=(val)=>{
+ setTotalQuantity(totalQuantity+val)
+//  dispatch({type:"INC_DEC_ITEM",payload:totalQuantity})
 }
+
+
+  return (
+    <TableContainer>
+      <Table variant="striped" colorScheme='teal' >
+        <TableCaption>Imperial to metric conversion factors</TableCaption>
+        <Thead >
+          <Tr fontSize="bold" fontWeight="20px" >
+            <Th>Items</Th>
+            <Th>Price</Th>
+            <Th>QUANTITY</Th>
+            <Th isNumeric>SUBTOTAL</Th>
+            <Th>DELETE</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {state.cart &&
+            state.cart.map((ele) => {
+              return (
+                <Tr>
+                  <Td>
+                    <Box
+                      width="200px"
+                      height="100%"
+                      display="flex"
+                      alignItems="center"
+                      gap="10px"
+                    >
+                      <Image
+                        width="100px"
+                        height="100px"
+                        src={ele.image_link}
+                        alt={ele.id}
+                      />
+                      <Text as="h1" width="50px" textAlign="justify">
+                        {ele.name}
+                      </Text>
+                    </Box>
+                  </Td>
+                  <Td>$ {ele.price}</Td>
+                  <Td>
+                  <QuantityBtn tcount={(val)=>handleTotalCount(val)}  />
+                  </Td>
+                  <Td isNumeric>25.4</Td>
+                  <Td>
+                    <CloseButton size="md" onClick={()=>dispatch({type:"DELETE_ITEM_FROM_CART",payload:ele.id})} />
+                  </Td>
+                </Tr>
+              );
+            })}
+        </Tbody>
+        <Tfoot>
+          <Tr>
+            {/* <Th>To convert</Th> */}
+            <Th></Th>
+            <Th fontSize="20px" fontWeight="bold" >Total</Th>
+            <Th fontSize="20px" fontWeight="bold" >Total Unit {state.total_item}</Th>
+            <Th isNumeric>SUBTOTAL</Th>
+          </Tr>
+        </Tfoot>
+      </Table>
+    </TableContainer>
+  );
+};
