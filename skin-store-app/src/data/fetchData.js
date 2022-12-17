b7a2cdaba9d8f777044733e1fb45eac7d4f5d90b
Satan Sharma <satansharma7549@gmail.com>
2022-12-17 23:59:37 +0530

    Update fetchData.js

--- skin-store-app/src/data/fetchData.js
+++ skin-store-app/src/data/fetchData.js
@@ -1,23 +1,16 @@
 import axios from "axios";
 
-const API="https://backenddataskinproducts-production.up.railway.app/api/skinproducts"
+const API =
+  "https://backenddataskinproducts-production.up.railway.app/api/skinproducts";
 
 export const fetchSingleProductByID = async (id) => {
-  return await axios.get(
-    `${API}/${id}`
-  );
+  return await axios.get(`${API}/${id}`);
 };
 
-export const fetchDataBySearch = async (query) => {
-  return await axios.get(
-    `${API}?q=${query}`
-  );
+export const fetchDataBySearch = async (query, page) => {
+  return await axios.get(`${API}?q=${query}&_page=${page}&_limit=10`);
 };
-
 
 export const SearchByCategory = async (product_type) => {
-  return await axios.get(
-    `${API}?product_type=${product_type}`
-  );
+  return await axios.get(`${API}?product_type=${product_type}`);
 };
-
