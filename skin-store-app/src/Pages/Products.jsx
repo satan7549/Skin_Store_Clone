16e37efa762409342b0a0c41303a90e0b16d861e
Satan Sharma <satansharma7549@gmail.com>
2022-12-17 21:35:14 +0530

    Update Products.jsx

--- skin-store-app/src/Pages/Products.jsx
+++ skin-store-app/src/Pages/Products.jsx
@@ -1,18 +1,27 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { Box } from '@chakra-ui/react'
-import  CardComp from "../Components/CardComp"
-import { fetchAllProducts, SearchByCategory } from '../data/fetchData';
-import { ProductContext } from '../Context/ProductContext/ProductContext';
+import React, { useContext, useEffect, useState } from "react";
+import { Box } from "@chakra-ui/react";
+import CardComp from "../Components/CardComp";
+import { fetchAllProducts, SearchByCategory } from "../data/fetchData";
+import { ProductContext } from "../Context/ProductContext/ProductContext";
+import { NavLink } from "react-router-dom";
 
 export const Products = () => {
-  const {state}=useContext(ProductContext);
-  const productType=state.categoryProduct
-  
+  const { state } = useContext(ProductContext);
+  const productType = state.categoryProduct;
+
   return (
-    <Box display="grid" gap="10px" p="10px" gridTemplateColumns="repeat(4,1fr)" gridTemplateRows="auto" >
-      {
-        productType.map((ele)=><CardComp key={ele.id} item={ele} /> )
-      }
+    <Box
+      display="grid"
+      gap="10px"
+      p="10px"
+      gridTemplateColumns="repeat(4,1fr)"
+      gridTemplateRows="auto"
+    >
+      {productType.map((ele) => (
+        <NavLink to={`/products/${ele.id}`} key={ele.id}>
+          <CardComp item={ele} />
+        </NavLink>
+      ))}
     </Box>
-  )
-}
+  );
+};
