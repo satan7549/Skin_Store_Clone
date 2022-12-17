1fddc57fa24ff99528b808fc51a41cff93664624
Satan Sharma <satansharma7549@gmail.com>
2022-12-18 04:07:32 +0530

    Update QuantyBtn.jsx

--- skin-store-app/src/Components/QuantyBtn.jsx
+++ skin-store-app/src/Components/QuantyBtn.jsx
@@ -2,23 +2,19 @@ import { Box, Text, Button } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import "../style/QuantyBtn.css";
 
-function QuantityBtn({ tcount }) {
+function QuantityBtn() {
   const [count, setCount] = useState(0);
 
   const Dec = () => {
     if (count > 0) {
       setCount(count - 1);
     }
-    // tcount(count)
+    
   };
   const Inc = () => {
     setCount(count + 1);
-    // tcount(count)
+    
   };
-
-  useEffect(() => {
-    tcount(count);
-  }, [count]);
 
   return (
     <Box className="qtyBtn">
