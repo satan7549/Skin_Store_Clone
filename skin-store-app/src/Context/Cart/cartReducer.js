3026d7ec6d2603ae2106afc3648e3807cfa1ae61
Satan Sharma <satansharma7549@gmail.com>
2022-12-17 23:59:08 +0530

    Update cartReducer.js

--- skin-store-app/src/Context/Cart/cartReducer.js
+++ skin-store-app/src/Context/Cart/cartReducer.js
@@ -1,34 +1,32 @@
-
-
-const cartReducer = (state,action) => {
-  switch(action.type){
-    case "ADD_TO_CART":{
+const cartReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_TO_CART": {
       //  console.log("cartreducer",action.payload)
-        let CartData;
-        CartData=action.payload
-        // ={id,image_link,name,price,price_sign,description,category,brand,product_type}
-        return{
-            ...state,
-            cart:[...state.cart,CartData]
-        }
-    }
-    case "DELETE_ITEM_FROM_CART":{
-      let filter=state.cart.filter((ele)=>ele.id !== action.payload)
-      return{
+      let CartData;
+      CartData = action.payload;
+      // ={id,image_link,name,price,price_sign,description,category,brand,product_type}
+      return {
         ...state,
-        cart:filter
-      }
+        cart: [...state.cart, CartData],
+      };
     }
-    case "INC_DEC_ITEM":{
-      console.log("reduce",action.payload);
-      return{
+    case "DELETE_ITEM_FROM_CART": {
+      let filter = state.cart.filter((ele) => ele.id !== action.payload);
+      return {
         ...state,
-        total_item:action.payload
-      }
+        cart: filter,
+      };
     }
-    default:{
-       return state;
+    case "INC_DEC_ITEM": {
+      console.log("reduce", action.payload);
+      return {
+        ...state,
+        total_item: action.payload,
+      };
+    }
+    default: {
+      return state;
     }
   }
-}
-export default cartReducer
+};
+export default cartReducer;
