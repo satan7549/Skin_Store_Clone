26fa5952bb420633f0841670548ead29643bd9e5
Satan Sharma <satansharma7549@gmail.com>
2022-12-19 04:38:41 +0530

    Create ImageSlider.js

---
+++ skin-store-app/src/Components/ImageSlider.js
@@ -0,0 +1,28 @@
+import { Box,Image } from "@chakra-ui/react";
+import React from "react";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import { cards } from "../data/fetchData";
+import { basicResponsive } from "../data/fetchData";
+
+export const ImageSlider = () => {
+  return (
+    <Carousel
+      infinite={true}
+      autoPlay={true}
+      autoPlaySpeed={3000}
+      transitionDuration={1000}
+      responsive={basicResponsive}
+      arrows={true}
+      showDots={true}
+      swipeable={false}
+      draggable={false}
+    >
+      {cards.map((ele, i) => (
+        <Box key={i} h="600px" >
+          <Image w="full" h="full" src={ele.image} alt={i} />
+        </Box>
+      ))}
+    </Carousel>
+  );
+};
