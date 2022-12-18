c7ea1fb31ad7593677c1db2c604c50092805b0ee
Satan Sharma <satansharma7549@gmail.com>
2022-12-19 04:39:24 +0530

    Update Home.jsx

--- skin-store-app/src/Pages/Home.jsx
+++ skin-store-app/src/Pages/Home.jsx
@@ -1,18 +1,32 @@
 import { Spacer } from "@chakra-ui/react";
 import React from "react";
-import Carasoul from "../Components/Carasoul";
-import  Footer  from "../Components/Footer";
-import { Products } from "./Products";
+import CategoryComp from "../Components/CategoryComp";
+import { Discover_Holiday_fun } from "../Components/Discover_Holiday_fun";
+import Footer from "../Components/Footer";
+import HomeImageBottom from "../Components/HomeImageBottom";
+import { ImageSlider } from "../Components/ImageSlider";
+import { OfferSlider } from "../Components/OfferSlider";
+import PartnerCompany from "../Components/PartnerCompany";
+import { ReactMultiCarasol } from "../Components/ReactMultiCarasol";
+import { TrandingOffer } from "../Components/TrandingOffer";
 
 export const Home = () => {
-
-
   return (
     <>
-    <Carasoul />
-    <Spacer />
-    <Footer />
+      <OfferSlider />
+      <Spacer h="20px" />
+      <ImageSlider />
+      <Spacer h="20px" />
+      <CategoryComp />
+      <Spacer h="20px" />
+      <Discover_Holiday_fun />
+      <Spacer h="20px" />
+      <TrandingOffer />
+      <Spacer h="20px" />
+      <PartnerCompany />
+      <Spacer h="20px" />
+      <HomeImageBottom />
+      <Footer />
     </>
-    
   );
 };
