8f39ba6993aa59e10a68a126d94ea9d46adadf7b
Satan Sharma <satansharma7549@gmail.com>
2022-12-19 04:39:13 +0530

    Create DiscoverHolidayfun.js

---
+++ skin-store-app/src/data/DiscoverHolidayfun.js
@@ -0,0 +1,461 @@
+export const Last_Minute_Gifting = [
+  {
+    id: 1048,
+    brand: "colourpop",
+    name: "Lippie Pencil",
+    price: "5.0",
+    price_sign: "$",
+    currency: "CAD",
+    image_link:
+      "https://cdn.shopify.com/s/files/1/1338/0845/collections/lippie-pencil_grande.jpg?v=1512588769",
+    product_link: "https://colourpop.com/collections/lippie-pencil",
+    website_link: "https://colourpop.com",
+    description:
+      "Lippie Pencil A long-wearing and high-intensity lip pencil that glides on easily and prevents feathering. Many of our Lippie Stix have a coordinating Lippie Pencil designed to compliment it perfectly, but feel free to mix and match!",
+    rating: null,
+    category: "pencil",
+    product_type: "lip_liner",
+    tag_list: ["cruelty free", "Vegan"],
+    created_at: "2018-07-08T23:45:08.056Z",
+    updated_at: "2018-07-09T00:53:23.301Z",
+    product_api_url:
+      "https://makeup-api.herokuapp.com/api/v1/products/1048.json",
+    api_featured_image:
+      "//s3.amazonaws.com/donovanbailey/products/api_featured_images/000/001/048/original/open-uri20180708-4-13okqci?1531093614",
+    product_colors: [
+      {
+        hex_value: "#B28378",
+        colour_name: "BFF Pencil",
+      },
+    ],
+  },
+  {
+    id: 1020,
+    brand: "marienatie",
+    name: "Eyeliner",
+    price: "0.0",
+    price_sign: "$",
+    currency: "USD",
+    image_link:
+      "https://static.thcdn.com/images/large/webp//productimg/1600/1600/12691024-3064813757049624.jpg",
+    product_link: "https://www.purpicks.com/product/marie-natie-eyeliner/",
+    website_link: "https://purpicks.com/",
+    description:
+      "An eyeliner that offers long lasting colour and a natural smooth finish.  Both jojoba oil and meadowfoam oil have amazing moisturizing properties to help repair skin.  Beeswax is known for its healing properties and will help to improve skin conditions.",
+    rating: null,
+    category: "pencil",
+    product_type: "eyeliner",
+    tag_list: ["purpicks", "CertClean"],
+    created_at: "2018-06-30T19:19:30.847Z",
+    updated_at: "2018-09-02T22:52:06.484Z",
+    product_api_url:
+      "https://makeup-api.herokuapp.com/api/v1/products/1020.json",
+    api_featured_image:
+      "//s3.amazonaws.com/donovanbailey/products/api_featured_images/000/001/020/original/open-uri20180630-4-y548d5?1530390373",
+    product_colors: [
+      {
+        hex_value: "#000000",
+        colour_name: "Black",
+      },
+      {
+        hex_value: "#393230",
+        colour_name: "Brown",
+      },
+      {
+        hex_value: "#353234",
+        colour_name: "Smoke",
+      },
+    ],
+  },
+  {
+    id: 1019,
+    brand: "maia's mineral galaxy",
+    name: "Mineral Eye Brow Liner Charcoal Brown",
+    price: "0.0",
+    price_sign: "$",
+    currency: "USD",
+    image_link:
+      "https://static.thcdn.com/images/large/webp//productimg/1600/1600/11385654-2284900271302533.jpg",
+    product_link:
+      "https://www.purpicks.com/product/maias-mineral-galaxy-mineral-eye-brow-liner-charcoal-brown/",
+    website_link: "https://purpicks.com/",
+    description: "",
+    rating: null,
+    category: "pencil",
+    product_type: "eyebrow",
+    tag_list: ["EWG Verified", "purpicks"],
+    created_at: "2018-06-30T19:19:30.782Z",
+    updated_at: "2018-09-02T22:52:06.474Z",
+    product_api_url:
+      "https://makeup-api.herokuapp.com/api/v1/products/1019.json",
+    api_featured_image:
+      "//s3.amazonaws.com/donovanbailey/products/api_featured_images/000/001/019/original/open-uri20180630-4-1h8zp2k?1530390372",
+    product_colors: [
+      {
+        hex_value: "#5B4439",
+        colour_name: "Charcoal Brown",
+      },
+      {
+        hex_value: "#000000",
+        colour_name: "Black",
+      },
+    ],
+  },
+  {
+    id: 1007,
+    brand: "c'est moi",
+    name: "Luminary Lip Crayon",
+    price: "0.0",
+    price_sign: "$",
+    currency: "USD",
+    image_link:
+      "https://static.thcdn.com/images/large/webp//productimg/1600/1600/12025046-6584958634095463.jpg",
+    product_link: "https://www.purpicks.com/product/luminary-lip-crayon/",
+    website_link: "https://purpicks.com/",
+    description:
+      "These nourishing and creamy lip crayons come in 6 perfectly pigmented shades that go from subtle to statement. This easy-to-use lip crayon delivers velvety application and perfectly pigmented color, plus a nourishing formula with a creamy, consistent texture designed to make your lips appear soft and smooth. Apply sparingly for a subtle look, or generously to make a statement—either way, your lips will feel as beautiful as they look! Made for Young Delicate Skin Types NO – Talc, Fragrance, Parabens, Phthalates, Sulfates, Mineral oil or Propylene Glycol Dermatologist Tested Clinically Tested On Sensitive Skin Hypoallergenic",
+    rating: null,
+    category: "pencil",
+    product_type: "lip_liner",
+    tag_list: ["purpicks", "EWG Verified", "Hypoallergenic", "No Talc"],
+    created_at: "2018-06-30T19:19:29.975Z",
+    updated_at: "2018-09-02T22:52:06.545Z",
+    product_api_url:
+      "https://makeup-api.herokuapp.com/api/v1/products/1007.json",
+    api_featured_image:
+      "//s3.amazonaws.com/donovanbailey/products/api_featured_images/000/001/007/original/open-uri20180630-4-1m2hvd7?1530390361",
+    product_colors: [
+      {
+        hex_value: "#B89093",
+        colour_name: "Fresh",
+      },
+      {
+        hex_value: "#CB7579",
+        colour_name: "True",
+      },
+    ],
+  },
+];
+
+export const New_Arrivals = [
+  {
+    id: 1045,
+    brand: "colourpop",
+    name: "No Filter Foundation",
+    price: "12.0",
+    price_sign: "$",
+    currency: "CAD",
+    image_link:
+      "https://static.thcdn.com/images/xsmall/webp//productimg/original/13971534-6365005595451348.jpg",
+    product_link: "https://colourpop.com/products/no-filter-matte-foundation",
+    website_link: "https://colourpop.com/products/no-filter-matte-foundation",
+    description:
+      "Developed for the Selfie Age, our buildable full coverage, natural matte foundation delivers flawless looking skin from day-to-night. The oil-free, lightweight formula blends smoothly and is easily customizable to create the coverage you want. Build it up or sheer it out, it was developed with innovative soft-blurring pigments to deliver true color while looking and feeling natural. The lockable pump is easy to use and keeps your routine mess-free! As always, 100% cruelty-free and vegan.",
+    rating: null,
+    category: "liquid",
+    product_type: "foundation",
+    tag_list: ["cruelty free", "Vegan"],
+    created_at: "2018-07-08T18:22:25.273Z",
+    updated_at: "2018-07-09T00:53:23.313Z",
+    product_api_url:
+      "https://makeup-api.herokuapp.com/api/v1/products/1045.json",
+    api_featured_image:
+      "//s3.amazonaws.com/donovanbailey/products/api_featured_images/000/001/045/original/open-uri20180708-4-4bvqii?1531074237",
+    product_colors: [
+      {
+        hex_value: "#F2DEC3",
+        colour_name: "Fair 05",
+      },
+      {
+        hex_value: "#F3DAC4",
+        colour_name: "Fair 10",
+      },
+    ],
+  },
+  {
+    id: 1043,
+    brand: "deciem",
+    name: "Serum Foundation",
+    price: "6.7",
+    price_sign: "$",
+    currency: "CAD",
+    image_link:
+      "https://static.thcdn.com/images/xsmall/webp//productimg/original/13909144-1235001632764440.jpg",
+    product_link: "https://deciem.com/product/rdn-serum-foundation-30-r-30ml",
+    website_link: "https://deciem.com/",
+    description:
+      "Serum Foundations are lightweight medium-coverage formulations available in a comprehensive shade range across 21 shades. These foundations offer moderate coverage that looks natural with a very lightweight serum feel. They are very low in viscosity and are dispensed with the supplied pump or with the optional glass dropper available for purchase separately if preferred. ",
+    rating: null,
+    category: "liquid",
+    product_type: "foundation",
+    tag_list: [
+      "water free",
+      "cruelty free",
+      "alcohol free",
+      "oil free",
+      "silicone free",
+      "Vegan",
+      "Gluten Free",
+    ],
+    created_at: "2018-07-06T23:16:12.698Z",
+    updated_at: "2018-09-02T22:52:06.882Z",
+    product_api_url:
+      "https://makeup-api.herokuapp.com/api/v1/products/1043.json",
+    api_featured_image:
+      "//s3.amazonaws.com/donovanbailey/products/api_featured_images/000/001/043/original/open-uri20180706-4-nszgw9?1530919194",
+    product_colors: [
+      {
+        hex_value: "#f4e5d6",
+        colour_name: "1.0P",
+      },
+      {
+        hex_value: "#f3e8d1",
+        colour_name: "1.0N",
+      },
+    ],
+  },
+  {
+    id: 1042,
+    brand: "deciem",
+    name: "Coverage Foundation",
+    price: "6.9",
+    price_sign: "$",
+    currency: "CAD",
+    image_link:
+      "https://static.thcdn.com/images/xsmall/webp//productimg/original/13232874-1014931468843100.jpg",
+    product_link:
+      "https://deciem.com/product/rdn-coverage-foundation-33-n-30ml",
+    website_link: "https://deciem.com/",
+    description:
+      "Coverage Foundations are full-coverage formulations available in a comprehensive shade range across 21 shades. These foundations contain higher pigment levels than our Serum Foundations but still offer a smooth finish that avoids the heavy makeup look that can make skin appear more aged. The texture is that of a lightweight, non-oily cream.",
+    rating: null,
+    category: "liquid",
+    product_type: "foundation",
+    tag_list: [
+      "water free",
+      "cruelty free",
+      "alcohol free",
+      "oil free",
+      "silicone free",
+      "Vegan",
+      "Gluten Free",
+    ],
+    created_at: "2018-07-06T22:27:54.541Z",
+    updated_at: "2018-09-02T22:52:06.865Z",
+    product_api_url:
+      "https://makeup-api.herokuapp.com/api/v1/products/1042.json",
+    api_featured_image:
+      "//s3.amazonaws.com/donovanbailey/products/api_featured_images/000/001/042/original/open-uri20180706-4-1e74943?1530916234",
+    product_colors: [
+      {
+        hex_value: "#f4e5d6",
+        colour_name: "1.0P",
+      },
+      {
+        hex_value: "#f3e8d1",
+        colour_name: "1.0N",
+      },
+    ],
+  },
+  {
+    id: 1041,
+    brand: "zorah biocosmetiques",
+    name: "Liquid Liner",
+    price: "0.0",
+    price_sign: "$",
+    currency: "USD",
+    image_link:
+      "https://static.thcdn.com/images/large/webp//productimg/1600/1600/13953430-7125000829805563.jpg",
+    product_link:
+      "https://www.purpicks.com/product/zorah-biocosmetiques-liquid-liner/",
+    website_link: "https://purpicks.com/",
+    description:
+      "<strong>12 hours of long-lasting</strong> intense color, transfer-free (leaves no trace on crease above the eyelid) <strong>Pure Light Capture®</strong> <strong>minerals</strong> deliver color and radiance. Silky lines and refreshingly light, Pure Argan eyeliner leaves a weightless feel on the eyelids.<p align='LEFT'>Natural cosmetic certified by Ecocert Greenlife according to Ecocert Standard available at: http://cosmetiques.ecocert.com</p>98% of the total ingredients are from natural origin 5% of total ingredients are from organic farming",
+    rating: null,
+    category: "liquid",
+    product_type: "eyeliner",
+    tag_list: ["purpicks", "EcoCert"],
+    created_at: "2018-06-30T19:19:32.312Z",
+    updated_at: "2018-09-02T22:52:06.723Z",
+    product_api_url:
+      "https://makeup-api.herokuapp.com/api/v1/products/1041.json",
+    api_featured_image:
+      "//s3.amazonaws.com/donovanbailey/products/api_featured_images/000/001/041/original/open-uri20180630-4-1huiv9y?1530390387",
+    product_colors: [
+      {
+        hex_value: "#000000",
+        colour_name: "Black",
+      },
+    ],
+  },
+];
+
+export const Best_Sellers = [
+  {
+    id: 1037,
+    brand: "sally b's skin yummies",
+    name: "B Glossy Lip Gloss",
+    price: "0.0",
+    price_sign: "$",
+    currency: "USD",
+    image_link:
+      "https://static.thcdn.com/images/xsmall/webp//productimg/original/11289609-8744892770898772.jpg",
+    product_link:
+      "https://www.purpicks.com/product/sally-bs-skin-yummies-b-glossy-lip-gloss/",
+    website_link: "https://purpicks.com/",
+    description:
+      "Intensify your natural lip color with Sally B's B Glossy Lip Gloss, an organic lip gloss that adds a silky smooth hint of color and shimmer to lips. Formulated with 98% certified organic, moisture-rich ingredients our B Glossy Lip Gloss works to enrich lip health for lips that feel good and look even better.",
+    rating: null,
+    category: "lip_gloss",
+    product_type: "lipstick",
+    tag_list: ["EWG Verified", "purpicks"],
+    created_at: "2018-06-30T19:19:32.053Z",
+    updated_at: "2018-09-02T22:52:06.687Z",
+    product_api_url:
+      "https://makeup-api.herokuapp.com/api/v1/products/1037.json",
+    api_featured_image:
+      "//s3.amazonaws.com/donovanbailey/products/api_featured_images/000/001/037/original/open-uri20180630-4-1fa1p2f?1530390384",
+    product_colors: [
+      {
+        hex_value: "#BF5D7F",
+        colour_name: "Every Blonde's Pink",
+      },
+      {
+        hex_value: "#D84945",
+        colour_name: "Primrose",
+      },
+    ],
+  },
+  {
+    id: 1030,
+    brand: "penny lane organics",
+    name: "Lip Gloss",
+    price: "0.0",
+    price_sign: "$",
+    currency: "USD",
+    image_link:
+      "https://static.thcdn.com/images/large/webp//productimg/1600/1600/12635437-1314911951568611.jpg",
+    product_link:
+      "https://www.purpicks.com/product/penny-lane-organics-lip-gloss/",
+    website_link: "https://purpicks.com/",
+    description:
+      "For fuller healthier lips! Long lasting colour! This colour is a good choice for any skin tone. It can be made lighter by wiping some off after application.Please note that our Vitamin E is extracted from non-GMO soy bean oil and therefore is gluten-free.",
+    rating: null,
+    category: "lip_gloss",
+    product_type: "lipstick",
+    tag_list: [
+      "purpicks",
+      "CertClean",
+      "Vegan",
+      "Chemical Free",
+      "Gluten Free",
+    ],
+    created_at: "2018-06-30T19:19:31.534Z",
+    updated_at: "2018-09-02T22:52:06.618Z",
+    product_api_url:
+      "https://makeup-api.herokuapp.com/api/v1/products/1030.json",
+    api_featured_image:
+      "//s3.amazonaws.com/donovanbailey/products/api_featured_images/000/001/030/original/open-uri20180630-4-ucbwbt?1530390380",
+    product_colors: [
+      {
+        hex_value: "#2F1513",
+        colour_name: "Black Cherry",
+      },
+      {
+        hex_value: "#4F1314",
+        colour_name: "Fiery Red",
+      },
+    ],
+  },
+  {
+    id: 1022,
+    brand: "marienatie",
+    name: "Lip Gloss",
+    price: "0.0",
+    price_sign: "$",
+    currency: "USD",
+    image_link:
+      "https://static.thcdn.com/images/large/webp//productimg/1600/1600/10915787-1224882139201778.jpg",
+    product_link: "https://www.purpicks.com/product/lip-gloss/",
+    website_link: "https://purpicks.com/",
+    description:
+      "MarieNatie offers a gluten free and vegan lip gloss providing rich colours and shine without the stickiness. The natural oils will offer moisturizing properties and lasting hydration for your lips. Contains grape seed oil to reduce aging and wrinkles and sunflower oil has the ability to heal skin. Vitamin E is a great antioxidant that will protect and repair skin.",
+    rating: null,
+    category: "lip_gloss",
+    product_type: "lipstick",
+    tag_list: ["purpicks", "CertClean"],
+    created_at: "2018-06-30T19:19:30.964Z",
+    updated_at: "2018-09-02T22:52:06.502Z",
+    product_api_url:
+      "https://makeup-api.herokuapp.com/api/v1/products/1022.json",
+    api_featured_image:
+      "//s3.amazonaws.com/donovanbailey/products/api_featured_images/000/001/022/original/data?1530390374",
+    product_colors: [
+      {
+        hex_value: "#D5A291",
+        colour_name: "Posh",
+      },
+      {
+        hex_value: "#D5A3A1",
+        colour_name: "Flair",
+      },
+    ],
+  },
+  {
+    id: 1009,
+    brand: "c'est moi",
+    name: "Reflect Lip Gloss",
+    price: "0.0",
+    price_sign: "$",
+    currency: "USD",
+    image_link:
+      "https://static.thcdn.com/images/large/webp//productimg/1600/1600/11287457-3854871985878184.jpg",
+    product_link: "https://www.purpicks.com/product/reflect-lip-gloss/",
+    website_link: "https://purpicks.com/",
+    description:
+      "These essential lip gloss shades are enriched with a blend of natural and organic oils to deliver the perfect amount of sheer color and shine to lips. Enriched with a blend of natural and organic oils, this essential gloss delivers a wash of sheer color and promises shine—not stickiness. Made for Young Delicate Skin Types NO – Talc, Fragrance, Parabens, Phthalates, Sulfates, Mineral oil or Propylene Glycol Dermatologist Tested Clinically Tested on Sensitive Skin, Hypoallergenic, Made with Natural & Organic Ingredients",
+    rating: null,
+    category: "lip_gloss",
+    product_type: "lipstick",
+    tag_list: ["purpicks", "EWG Verified", "Hypoallergenic", "No Talc"],
+    created_at: "2018-06-30T19:19:30.116Z",
+    updated_at: "2018-09-02T22:52:06.562Z",
+    product_api_url:
+      "https://makeup-api.herokuapp.com/api/v1/products/1009.json",
+    api_featured_image:
+      "//s3.amazonaws.com/donovanbailey/products/api_featured_images/000/001/009/original/open-uri20180630-4-xznfso?1530390363",
+    product_colors: [
+      {
+        hex_value: "#DF585A",
+        colour_name: "Rise",
+      },
+      {
+        hex_value: "#E4CFCE",
+        colour_name: "Opulence",
+      },
+    ],
+  },
+];
+
+export const trandingOffer = [
+  {
+    src: "https://static.thcdn.com/images/small/webp/widgets/121-us/39/17_1009_CucumberExtract_US17_1009_CucumberExtract_US-014239.jpg",
+
+    h: "SkinCeutivcals Gift",
+
+    p: "Receive a SkinCeuticals Triple Lipid Restore 2:4:2 Sample when you spend $150 or more on the brand.",
+  },
+  {
+    src: "https://static.thcdn.com/images/small/webp/widgets/121-us/39/0712-STDCRE-38046-SS-MH-Photography-July-22-REMAINING-BATCHING-Shot8-600x600-063639.png",
+
+    h: "25% off Elemis with code SINGLES",
+
+    p: "Elemis skincare products are carefully formulated to maximize the potential of your skin and restore its best qualities to enhance your natural beauty.",
+  },
+  {
+    src: "https://static.thcdn.com/images/small/webp/widgets/121-us/10/0224-STDCRE-32153-SS-BME-Skinstore-March-2022-Photography-Batching-Shot_14-600x600-091010.jpg",
+
+    h: "Up to 50% off TriPollar",
+    p: "TriPollar uses clinical expertise to create home-use devices that encourage skin regeneration and enhance natural beauty.",
+  },
+];
