d7b59da7392178c05224ced27124292c891dd6dd
Satan Sharma <satansharma7549@gmail.com>
2022-12-19 04:38:55 +0530

    Create ReactMultiCarasol.jsx

---
+++ skin-store-app/src/Components/ReactMultiCarasol.jsx
@@ -0,0 +1,39 @@
+// import React from "react";
+// import Carousel from "react-multi-carousel";
+// import "react-multi-carousel/lib/styles.css";
+// import { responsive } from "../data/fetchData";
+// export const cards = [
+//   { text: "1", bg: "red" },
+//   { text: "2", bg: "blue" },
+//   { text: "3", bg: "black" },
+//   { text: "4", bg: "teal" },
+//   { text: "5", bg: "orange" },
+//   { text: "6", bg: "yellow" },
+//   { text: "7", bg: "pink" },
+// ];
+
+// export const ReactMultiCarasol = () => {
+  
+
+//   return (
+//     <Carousel
+//       infinite={true}
+//       autoPlay={true}
+//       autoPlaySpeed={1000}
+//       responsive={responsive}
+//     >
+//       {cards.map((ele, i) => (
+//         <div
+//           key={i}
+//           style={{
+//             border: `solid 2px ${ele.bg}`,
+//             height: "400px",
+//             gap: "10px",
+//           }}
+//         >
+//           {ele.text}
+//         </div>
+//       ))}
+//     </Carousel>
+//   );
+// };
