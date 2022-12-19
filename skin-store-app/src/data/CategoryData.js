8577982ca20c120ef6cc0adabbf116b6daedb7c1
Satan Sharma <satansharma7549@gmail.com>
2022-12-19 09:22:36 +0530

    Update CategoryData.js

--- skin-store-app/src/data/CategoryData.js
+++ skin-store-app/src/data/CategoryData.js
@@ -34,62 +34,30 @@ const CategoryData = [
 const c_avg = [
   {
     src: "https://static.thcdn.com/images/small/webp/widgets/121-us/01/Page-001-025201.png",
+    category:"liquid"
   },
   {
     src: "https://static.thcdn.com/images/small/webp/widgets/121-us/09/Page-002-025209.png",
+    category:"pencil"
   },
   {
     src: "https://static.thcdn.com/images/small/webp/widgets/121-us/17/Page-003-025217.png",
+    category:"lip_gloss"
   },
   {
     src: "https://static.thcdn.com/images/small/webp/widgets/121-us/21/Page-004-025221.png",
+    category:"palette"
   },
   {
     src: "https://static.thcdn.com/images/small/webp/widgets/121-us/40/Page-005-025240.png",
+    category:"lipstick"
   },
   {
     src: "https://static.thcdn.com/images/small/webp/widgets/121-us/58/Page-006-025258.png",
+    category:"concealer"
   },
 ];
 
-///Trending Offers
 
-const trandingOffers = [
-  {
-    img_src:
-      "https://static.thcdn.com/images/small/webp/widgets/121-us/39/17_1009_CucumberExtract_US17_1009_CucumberExtract_US-014239.jpg",
-    h: "SkinCeutivcals Gift",
-    p: "Receive a SkinCeuticals Triple Lipid Restore 2:4:2 Sample when you spend $150 or more on the brand.",
-  },
-  {
-    img_src:
-    "https://static.thcdn.com/images/small/webp/widgets/121-us/39/0712-STDCRE-38046-SS-MH-Photography-July-22-REMAINING-BATCHING-Shot8-600x600-063639.png",
-    h: "25% off Elemis with code SINGLES",
-    p: "Elemis skincare products are carefully formulated to maximize the potential of your skin and restore its best qualities to enhance your natural beauty.",
-  },
-  {
-    img_src:
-    "https://static.thcdn.com/images/small/webp/widgets/121-us/27/SkinStore_Q4_Photography_2022_-_October_-_THG0035163_-_BATCHING_shot8600x600-090227.jpg",
-    h: "Brand of the Month: Obagi Medical",
-    p:"Obagi crafts advanced skincare products that help unleash skin’s full potential with an extensive portfolio designed to brighten, protect, and enhance skin tone and texture.",
-  },
-  {
-    img_src:
-      "https://static.thcdn.com/images/small/webp/widgets/121-us/39/17_1009_CucumberExtract_US17_1009_CucumberExtract_US-014239.jpg",
-    h: "SkinCeutivcals Gift",
-    p: "Receive a SkinCeuticals Triple Lipid Restore 2:4:2 Sample when you spend $150 or more on the brand.",
-  },
-];
 
-// "Lip Liner",
-//   "Lipstick",
-//   "Foundation",
-//   "Eyeliner",
-//   "Eyeshadow",
-//   "Blush",
-//   "Bronzer",
-//   "Mascara",
-//   "Eyebrow",
-//   "Nail Polish",
-//     "pencil"
 export { CategoryData, c_avg };
