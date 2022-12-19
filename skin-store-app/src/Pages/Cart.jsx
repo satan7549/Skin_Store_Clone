96da89d14545787ce7ab9836b7d50c0bff35d705
Satan Sharma <satansharma7549@gmail.com>
2022-12-19 09:22:40 +0530

    Update Cart.jsx

--- skin-store-app/src/Pages/Cart.jsx
+++ skin-store-app/src/Pages/Cart.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useState } from "react";
 import { CartContext } from "../Context/Cart/CartContextProvider";
 import {
   Table,
