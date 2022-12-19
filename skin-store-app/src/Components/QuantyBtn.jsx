7831165e07b44301d58ba7170e5f81410f5eef38
Satan Sharma <satansharma7549@gmail.com>
2022-12-19 09:22:25 +0530

    Update QuantyBtn.jsx

--- skin-store-app/src/Components/QuantyBtn.jsx
+++ skin-store-app/src/Components/QuantyBtn.jsx
@@ -1,5 +1,5 @@
 import { Box, Text, Button } from "@chakra-ui/react";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "../style/QuantyBtn.css";
 
 function QuantityBtn() {
