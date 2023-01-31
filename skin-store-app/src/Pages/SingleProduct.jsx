7fcf2900e7223cd1b282849c271fcfb5875a093c
Satan Sharma <satansharma7549@gmail.com>
2023-02-01 03:36:24 +0530

    update

--- skin-store-app/src/Pages/SingleProduct.jsx
+++ skin-store-app/src/Pages/SingleProduct.jsx
@@ -13,6 +13,9 @@ import {
   AccordionIcon,
   Heading,
   Button,
+  SimpleGrid,
+  GridItem,
+  useMediaQuery,
 } from "@chakra-ui/react";
 import { CartContext } from "../Context/Cart/CartContextProvider";
 
@@ -21,6 +24,7 @@ export const SingleProduct = () => {
   const { id } = useParams();
   const [data, setData] = useState({});
   const [click, setClick] = useState(0);
+  const [isLargerThan475] = useMediaQuery("(max-width: 475px)");
 
   const handleAddToCart = (item) => {
     setClick(1);
@@ -34,7 +38,6 @@ export const SingleProduct = () => {
 
   return (
     <Box
-      margin="auto"
       h="full"
       display="flex"
       flexDirection="column"
@@ -42,49 +45,96 @@ export const SingleProduct = () => {
       bg="blackAlpha.100"
     >
       {/* <Container mt="50px"  > */}
-      <HStack
-        spacing={8}
-        justifyContent="space-around"
-        shadow="md"
-        direction="row"
-        p={4}
-      >
-        <Box p={5} shadow="md" borderWidth="1px">
-          <Image src={data.image_link} alt={data.id} />
-        </Box>
-        <Box p={5} w="md" h="md">
-          <VStack>
-            <Box
-              display="flex"
-              flexDirection="column"
-              textAlign="start"
-              gap={10}
-              justifyContent="space-around"
-            >
-              <Heading fontSize="xl">Name :- {data.name}</Heading>
-              <Heading fontSize="xl">Brand :- {data.brand}</Heading>
-              <Heading fontSize="xl">Category :- {data.category}</Heading>
-              <Heading fontSize="xl">
-                Product Type :- {data.product_type}
-              </Heading>
-              <Heading fontSize="xl">Price :- ${data.price}</Heading>
-
-              <Button
-                width="100%"
-                p="-1"
-                borderRadius="none"
-                colorScheme="black"
-                _hover={{ bg: "black", color: "white" }}
-                variant="outline"
-                disabled={click === 1 ? true : false}
-                onClick={() => handleAddToCart(data)}
+      {isLargerThan475 ? (
+        <VStack
+          // spacing={8}
+          justifyContent="space-around"
+          shadow="md"
+          direction="row"
+          p={4}
+        >
+          <Box p={5} shadow="md" borderWidth="1px">
+            <Image src={data.image_link} alt={data.id} />
+          </Box>
+          <Box p={5} w="md" h="md">
+            <VStack>
+              <Box
+                display="flex"
+                flexDirection="column"
+                textAlign="start"
+                gap={10}
+                justifyContent="space-around"
               >
-                ADD TO CART
-              </Button>
-            </Box>
-          </VStack>
-        </Box>
-      </HStack>
+                <Heading fontSize="xl">Name :- {data.name}</Heading>
+                <Heading fontSize="xl">Brand :- {data.brand}</Heading>
+                <Heading fontSize="xl">Category :- {data.category}</Heading>
+                <Heading fontSize="xl">
+                  Product Type :- {data.product_type}
+                </Heading>
+                <Heading fontSize="xl">Price :- ${data.price}</Heading>
+
+                <Button
+                  width="100%"
+                  p="-1"
+                  borderRadius="none"
+                  colorScheme="black"
+                  _hover={{ bg: "black", color: "white" }}
+                  variant="outline"
+                  disabled={click === 1 ? true : false}
+                  onClick={() => handleAddToCart(data)}
+                >
+                  ADD TO CART
+                </Button>
+              </Box>
+            </VStack>
+          </Box>
+        </VStack>
+      ) : (
+        <HStack
+          spacing={8}
+          justifyContent="space-around"
+          shadow="md"
+          direction="row"
+          p={4}
+        >
+          <Box p={5} shadow="md" borderWidth="1px">
+            <Image src={data.image_link} alt={data.id} />
+          </Box>
+          <Box p={5} w="md" h="md">
+            <VStack>
+              <Box
+                display="flex"
+                flexDirection="column"
+                textAlign="start"
+                gap={10}
+                justifyContent="space-around"
+              >
+                <Heading fontSize="xl">Name :- {data.name}</Heading>
+                <Heading fontSize="xl">Brand :- {data.brand}</Heading>
+                <Heading fontSize="xl">Category :- {data.category}</Heading>
+                <Heading fontSize="xl">
+                  Product Type :- {data.product_type}
+                </Heading>
+                <Heading fontSize="xl">Price :- ${data.price}</Heading>
+
+                <Button
+                  width="100%"
+                  p="-1"
+                  borderRadius="none"
+                  colorScheme="black"
+                  _hover={{ bg: "black", color: "white" }}
+                  variant="outline"
+                  disabled={click === 1 ? true : false}
+                  onClick={() => handleAddToCart(data)}
+                >
+                  ADD TO CART
+                </Button>
+              </Box>
+            </VStack>
+          </Box>
+        </HStack>
+      )}
+
       <VStack>
         <Accordion defaultIndex={[0]} allowMultiple>
           <AccordionItem>
