bfe36d5d88595e658ac7dc0d886c6707130a82ed
Satan Sharma <satansharma7549@gmail.com>
2023-04-05 19:49:42 +0530

    Update HomeImageBottom.jsx

--- skin-store-app/src/Components/HomeImageBottom.jsx
+++ skin-store-app/src/Components/HomeImageBottom.jsx
@@ -1,18 +1,17 @@
-import React from 'react'
-import "../style/HomeImageBottom.css"
-const img=[
-    "https://static.thcdn.com/images/large/webp/widgets/121-us/36/original-Skinstore_August_Referral_-_Untitled_Page_%282%29-022536.png",
-    "https://static.thcdn.com/images/large/webp/widgets/121-us/37/original-0308-STDCRE32346-SS-AH-SkinStore-Concierge-Banners-770x200-084637.jpg"
-]
- const HomeImageBottom = () => {
+import React from "react";
+import { Box, Image } from "@chakra-ui/react";
+const img = [
+  "https://static.thcdn.com/images/large/webp/widgets/121-us/36/original-Skinstore_August_Referral_-_Untitled_Page_%282%29-022536.png",
+  "https://static.thcdn.com/images/large/webp/widgets/121-us/37/original-0308-STDCRE32346-SS-AH-SkinStore-Concierge-Banners-770x200-084637.jpg",
+];
+const HomeImageBottom = () => {
   return (
-    <div className='HomeImageBottom-main-div' >
-        {
-            img.map((ele,i)=><img key={i} src={ele} alt={i} />)
-        }
-        
-    </div>
-  )
-}
+    <Box>
+      {img.map((ele, i) => (
+        <Image key={i} src={ele} alt={i} />
+      ))}
+    </Box>
+  );
+};
 
-export default HomeImageBottom
+export default HomeImageBottom;
