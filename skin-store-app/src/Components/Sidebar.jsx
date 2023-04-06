4c0721ad260e43657edc87067f65b3223e6ddeee
Satan Sharma <satansharma7549@gmail.com>
2023-04-06 20:42:53 +0530

    Update Sidebar.jsx

--- skin-store-app/src/Components/Sidebar.jsx
+++ skin-store-app/src/Components/Sidebar.jsx
@@ -22,18 +22,14 @@ export const Sidebar = () => {
   const btnRef = React.useRef();
   const { dispatch } = useContext(ProductContext);
   const [productType, setProductTtype] = useState("");
-  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    setLoading(true);
     try {
       SearchByCategory(productType).then((res) => {
         const products = res.data;
-        setLoading(false);
         return dispatch({ type: "GET_CATEGORY_PRODUCTS", payload: products });
       });
     } catch (error) {
-      setLoading(false);
       console.log("error:", error);
     }
   }, [productType]);
